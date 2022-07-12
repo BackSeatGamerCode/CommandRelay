53d67d2fd741b518f2aa8b1feb5826cc23908637
Christopher <cpsuperstoreinc@gmail.com>
2022-07-11 22:17:31 -0400

    Improved discord bot kill event

--- source/screens/modes/discord_bot.py
+++ source/screens/modes/discord_bot.py
@@ -1,15 +1,22 @@
-import TwitchPyRC
-import discord
 import asyncio
-from signal import SIGINT, SIGTERM
 
-import source.exceptions as exceptions
+import discord
+
 import source.screens.modes.base_mode as base_mode
 import source.setting as setting
 
 
 class DiscordBot(base_mode.BaseMode):
     def __init__(self, *args, **kwargs):
+        self.bot: discord.Client = None
+
+        super().__init__(*args, **kwargs)
+
+    def startup(self):
+        self.get_additional_settings("discord_bot", [
+            setting.Setting("Bot Token", "token")
+        ])
+
         self.bot = discord.Client()
 
         @self.bot.event
@@ -22,19 +29,10 @@ class DiscordBot(base_mode.BaseMode):
                 if response is not None:
                     await message.channel.send(response)
 
-        super().__init__(*args, **kwargs)
-
-    def startup(self):
-        self.get_additional_settings("discord_bot", [
-            setting.Setting("Bot Token", "token")
-        ])
-
     def _relay_commands(self):
         self.bot.run(self.additional_settings["token"])
 
     def teardown(self):
-        # self.bot.loop.close()
-        for signal in [SIGINT, SIGTERM]:
-            self.bot.loop.add_signal_handler(signal, lambda x: x)
-
-        self.bot.loop.close()
+        loop = asyncio.get_event_loop()
+        loop.create_task(self.bot.close())
+        loop.stop()
